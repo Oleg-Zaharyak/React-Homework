e644eb993cb3ce39cf758fbfc8ef932b7e7d7456
Oleg Zaharyak <oleg.zaharyak@gmail.com>
2022-05-27 23:03:01 +0300

    homewor_react1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,55 @@
 // import logo from './logo.svg';
-// import './App.css';
+import "./App.css";
+const Header = (props) => {
+  return <h1>Feedback project from United Software by {props.name}</h1>;
+};
+
+const NumberList = (props) => {
+  let arr = [];
+  let res;
+  for (let i = 1; i <= props.count; i++) {
+    arr.push(i);
+  }
+  res = arr.map((a) => (
+    <li>
+      <input type="radio" name="radio" /> {a}
+    </li>
+  ));
+  return res;
+};
+
+const Feedback = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Write your feedback" />
+      <button>Send feedback</button>
+    </div>
+  );
+};
+
+const Question = (props) => {
+  return (
+    <div className="flexDiv">
+      <h2>How would you rate our cours?</h2>
+      <ul>
+        <NumberList count={10} />
+      </ul>
+      <Feedback />
+    </div>
+  );
+};
+
+const Footer = (props) => {
+  return <h2>© Made by {props.name}</h2>;
+};
 
 function App() {
   return (
-    <p>Hello world</p>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="flexDiv">
+      <Header name="Oleg Zakhariak" />
+      <Question />
+      <Footer name="Oleg Zakhariak" />
+    </div>
   );
 }
 
