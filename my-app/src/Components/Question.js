3ef94cae5ffb48c9957e42bedd9cbe54fc97a1ed
Oleg Zaharyak <oleg.zaharyak@gmail.com>
2022-05-29 23:11:52 +0300

    homework_react_2

--- my-app/src/Components/Question.js
+++ my-app/src/Components/Question.js
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NumberList = (props) => {
-  const arr = Array.from({ length: props.count }, (_, k) => k + 1); 
-  const list = arr.map((numb,index) => (
-    <li key = {index}>
-      <input type="radio" name="radio" /> {numb}
-    </li>
-  ));
-  return <ul>{list}</ul>;
-};
 
-const Feedback = () => {
-  return (
-    <div>
-      <input type="text" placeholder="Write your feedback" />
-      <button>Send feedback</button>
-    </div>
-  );
-};
 
 export const Question = (props) => {
+  
+  const NumberList = (props) => {
+    const value = props.count;
+    const arr = Array.from({ length: props.count }, (_, k) =>
+      <li key = {k+1}>
+        <input type="radio" name="radio"  id = {k+1}  defaultChecked = {value === k + 1}/>
+        <label htmlFor={k+1}>{k+1} </label> 
+      </li>
+    ); 
+    return <ul>{arr}</ul>;
+  };
+  
+ 
+  const [output, setOutput] = useState([{number: "", textFeed: ""}])
+  const [triger, setTriger] = useState(true)
+
+  const target = (event) =>{
+    let targ = event.target.value;
+    if (targ !== null){
+     setTriger(false);
+    }
+    else{
+     setTriger(true);
+    }
+  }
+ 
+
+  const sendFeedback = ()=> {
+      setTriger(true);
+      let text = document.getElementById("input").value;
+      let res = document.querySelector("[checked]").id;
+      setOutput([...output,{ number: res, textFeed: text } ])
+      }
+      
+  const Feedback = () => {
+    return (
+      <div>
+        <input id = "input" type="text"  onChange={target}  placeholder="Write your feedback" />
+        <button disabled = {triger} onClick={sendFeedback} >Send feedback</button>
+      </div>
+    );
+  };
+
+  const TextFeedback = ()=>{
+    return output.map(elem=>(
+      <h3 key = {elem.number} >{elem.number}  {elem.textFeed}</h3>)
+    )
+  }
+
+
   return (
     <div className="flexDiv">
       <h2>How would you rate our cours?</h2>
-      <NumberList count={10} />
+      <NumberList count={10}/>
       <Feedback />
+      <TextFeedback/>
     </div>
-  );
+  );  
 };
